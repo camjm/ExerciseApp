8d12611142b8830e1be9c0e33eb5749e0e1ca1fb
Cam <cam@mackay.net.nz>
2017-12-11 09:01:38 +1300

    quotes page

--- ExerciseApp/ExerciseApp/App.xaml.cs
+++ ExerciseApp/ExerciseApp/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace ExerciseApp
 {
@@ -13,7 +8,7 @@ namespace ExerciseApp
         {
             InitializeComponent();
 
-            MainPage = new ExerciseApp.MainPage();
+            MainPage = new QuotesPage();
         }
 
         protected override void OnStart()
---
+++ ExerciseApp/ExerciseApp/QuotesPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Xamarin.Forms;
+
+namespace ExerciseApp
+{
+    public partial class QuotesPage : ContentPage
+    {
+        private int index = 0;
+
+        private string[] quotes = new string[]
+        {
+            "Quote 1",
+            "Quote 2",
+            "Quote 3",
+            "Quote 4",
+            "Quote 5"
+        };
+
+        public QuotesPage()
+        {
+            InitializeComponent();
+            
+            //// Alternative to using XAML
+            //switch (Device.RuntimePlatform)
+            //{
+            //    case Device.Android:
+            //        Padding = new Thickness(20, 30, 20, 30);
+            //        break;
+            //    case Device.WinPhone:
+            //        Padding = new Thickness(20, 40, 20, 20);
+            //        break;
+            //}
+            
+            slider.Value = 20;
+            quote.Text = quotes[index];
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            index++;
+            if (index >= quotes.Length)
+            {
+                index = 0;
+            }
+
+            quote.Text = quotes[index];
+        }
+    }
+}
